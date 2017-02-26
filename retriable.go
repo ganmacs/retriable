9a2761c3e852fe8db9f57c568deea76c0e71f867
ganmacs <ganmacs@gmail.com>
2017-02-26 23:19:18 +0900

    Add MaxElapsedTime

--- retriable.go
+++ retriable.go
@@ -1,39 +1,71 @@
 package retriable
 
 import (
+	"errors"
 	"github.com/ganmacs/retriable/backoff"
 	"time"
 )
 
 const (
-	defaultRetries = 3
+	defaultRetries        = 3
+	defaultMaxElapsedTime = 15 * time.Minute
 )
 
 type Operation func() error
 
 type Options struct {
-	operation Operation
-	retries   int
+	operation      Operation
+	retries        int
+	maxElapsedTime time.Duration
+}
+
+type clock struct {
+	startTime time.Time
+}
+
+func newClock() *clock {
+	return &clock{
+		startTime: time.Now(),
+	}
+}
+
+func (c *clock) getElapsedTime() time.Duration {
+	return time.Now().Sub(c.startTime)
 }
 
 func Retry(op Operation) error {
 	opt := &Options{
-		operation: op,
-		retries:   defaultRetries,
+		operation:      op,
+		retries:        defaultRetries,
+		maxElapsedTime: defaultMaxElapsedTime,
 	}
 
 	return doRetry(backoff.NewExponentialBackOff(), opt)
 }
 
+func RetryWithOptions(op Operation, opt *Options) error {
+	opt.operation = op
+	return doRetry(backoff.NewExponentialBackOff(), opt)
+}
+
 func doRetry(bo backoff.BackOff, opt *Options) error {
 	var retries = opt.retries
-
 	var next time.Duration
 	var err error
+
+	var clock = newClock()
+
+	if retries < 1 {
+		return errors.New("retires should be 1 or more")
+	}
 
 	for i := 0; i < retries; i++ {
 		if err = opt.operation(); err == nil {
 			return nil
+		}
+
+		if opt.maxElapsedTime < clock.getElapsedTime() {
+			return errors.New("Timeout")
 		}
 
 		// TODO fix -1
--- retriable_test.go
+++ retriable_test.go
@@ -3,6 +3,7 @@ package retriable
 import (
 	"errors"
 	"testing"
+	"time"
 )
 
 func TestRetry(t *testing.T) {
@@ -36,5 +37,25 @@ func TestFailRetry(t *testing.T) {
 
 	if i != 3 {
 		t.Errorf("should be 3 but, %v", i)
+	}
+}
+
+func TestMaxElapsedTimeRetry(t *testing.T) {
+	fn := func() error {
+		time.Sleep(2)
+		return errors.New("error")
+	}
+
+	err := RetryWithOptions(fn, &Options{
+		retries:        3,
+		maxElapsedTime: 500 * time.Millisecond,
+	})
+
+	if err == nil {
+		t.Errorf("should have error")
+	}
+
+	if err.Error() != "Timeout" {
+		t.Errorf("unexpected error: %s", err.Error())
 	}
 }
